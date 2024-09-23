897392c6bc8e77c19288265451d2e5f2e754aa56
erickiwi123 <ericferreiraassiz@gmail.com>
2024-09-23 10:20:11 -0300

    55

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(3));
+
 function areatriangulo(base,altura,){
     return base*altura/2;
 }
-console.log(areatriangulo(8,5))
+console.log(areatriangulo(5,8))
