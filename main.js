25ec12e38294736f316cd4247836bba961b1cbfa
erickiwi123 <ericferreiraassiz@gmail.com>
2024-09-30 10:31:44 -0300

    r

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-;
+let estados=['PR','RS','SC','SP','MT','RJ','MG','ES','AC','CE','GO','AM','AP','AL','PA','RN','DF','BA','PE','TO','MS','RO','SE','RR','MA','PI','PB']
+let i=0; 
+while(i<estados.length){
+    console.log(i);
+    console.log(estados[i]);
+    i=i+i;
 
+}
 
-
-
- let estados=[`]
-
+let idade=23;
+if (idade>17){
+    console.log('ja pode dirigir');
+}
+else{
+    console.log('ilegal dirigir')
+}
