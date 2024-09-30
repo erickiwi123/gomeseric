850f0b330e8fd3646e2cb21abd35c3efa6191f40
erickiwi123 <ericferreiraassiz@gmail.com>
2024-09-30 09:32:12 -0300

    e

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-// alert('deu certo')
-// alert('ericlisson')
-function soma(num1,num2){
-    return num1+num2;
-}
+;
 
-console.log(soma(2,3));
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(3));
 
-function areatriangulo(base,altura,){
-    return base*altura/2;
-}
-console.log(areatriangulo(5,8))
 
-let nome=prompt('abner merda?'/);
-let nome=prompt('helenol cagada?'/);
-let nome=prompt('maria escuro muito estressad?e cagada?'/);
-document.Write ('abner merda?');
+
+ let estados=[`]
+
